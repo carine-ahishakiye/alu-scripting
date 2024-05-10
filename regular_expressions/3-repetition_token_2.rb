08d1871ba4c8fb69e06e7537ca7009eacc700c78
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-05-10 08:38:38 -0700

    3-repetition_token_2.rb

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-# 3-repetition_token_2.rb
-
-# Define the regular expression pattern
-pattern = /hb(t{3,6})n/
-
-# Get the argument passed to the script
-input_string = ARGV[0]
-
-# Check if the input string matches the pattern
-if input_string.match?(pattern)
-  puts "Correct output with #{input_string}"
-else
-  puts "Incorrect output with #{input_string}"
-end
+puts ARGV[0].scan(/hbt+n/).join
