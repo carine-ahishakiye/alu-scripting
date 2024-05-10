c84d18c215ebb74f3eb2dd8f4f788c736a89d7dd
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-05-10 01:58:14 -0700

    0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/school/).join
+
+# Check if the argument is provided
+if ARGV.empty?
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Get the argument from the command line
+string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Scan the string for all occurrences of the pattern
+matches = string.scan(pattern)
+
+# Print all the matched occurrences
+matches.each do |match|
+  puts match
+end
