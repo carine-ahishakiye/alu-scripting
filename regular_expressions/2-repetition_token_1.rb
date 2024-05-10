8bf0e3e801b8200974a2dd3f8e3018eb453bdffd
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-05-10 08:40:26 -0700

    2-repetition_token_1.rb

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# 1-repetition_token_0.rb
-
-# Define the regular expression pattern
-pattern = /token[ ]*#0/
-
-# Get the argument passed to the script
-input_string = ARGV[0]
-
-# Check if the input string matches the pattern
-if input_string.match?(pattern)
-  puts "Input string matches the pattern!"
-else
-  puts "Input string does not match the pattern."
-end
-
+puts ARGV[0].scan(/hb?tn/).join
