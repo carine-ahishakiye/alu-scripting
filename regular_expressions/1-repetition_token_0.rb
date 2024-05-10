80cff19cdb30e2ba101484bf3fc4fbb207e3e4a5
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-05-10 08:03:51 -0700

    1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,20 @@
-#!/usr/bin/env ruby
-#puts ARGV[0].scan(/hbt{2,5}n/).join
+# 1-repetition_token_0.rb
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: ruby 1-repetition_token_0.rb <string>"
+  exit(1)
+end
+
+# Get the argument from the command line
+input_string = ARGV[0]
+
+# Regular expression pattern to match the specified cases
+pattern = /mandatory/
+
+# Match the input string against the pattern
+if input_string.match?(pattern)
+  puts "The input string '#{input_string}' matches the pattern '#{pattern}'."
+else
+  puts "The input string '#{input_string}' does not match the pattern '#{pattern}'."
+end
