a8ad881790962972b166d0d2a6f773fb6b71c651
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-05-10 01:27:42 -0700

    3-repetition_token_2.rb

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -3,15 +3,15 @@
 # 3-repetition_token_2.rb
 
 # Define the regular expression pattern
-pattern = /your_regular_expression_here/
+pattern = /hb(t{3,6})n/
 
 # Get the argument passed to the script
 input_string = ARGV[0]
 
 # Check if the input string matches the pattern
 if input_string.match?(pattern)
-  puts "Input string matches the pattern!"
+  puts "Correct output with #{input_string}"
 else
-  puts "Input string does not match the pattern."
+  puts "Incorrect output with #{input_string}"
 end
 
