ffc50c71e4a8ab31ef672648d5fc2fb0c4ceb2d6
Carine-Ahishakiye <c.yibukabay@alustudent.com>
2024-07-05 10:47:29 +0000

    How many subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-""" Returns the number of subscribers for a given subreddit. """
+"""Return the number of subscribers of a given subreddit"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditScript/0.1'}
-    # Create the URL for the Reddit API request
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
     try:
-        # Make the request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            # Return the number of subscribers
-            return data['data']['subscribers']
-        else:
-            # If the subreddit is invalid, return 0
-            return 0
-    except requests.RequestException:
-        # In case of any request exception, return 0
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
         return 0
