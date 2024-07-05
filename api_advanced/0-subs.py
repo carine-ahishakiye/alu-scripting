2518d2a2c4742527b0a62b0086dec2bfdd275217
Carine-Ahishakiye <c.yibukabay@alustudent.com>
2024-07-05 10:34:32 +0000

    How many subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,22 +5,23 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers for a given subreddit. """
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = \
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    else:
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'MyRedditScript/0.1'}
+    # Create the URL for the Reddit API request
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    try:
+        # Make the request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            # Return the number of subscribers
+            return data['data']['subscribers']
+        else:
+            # If the subreddit is invalid, return 0
+            return 0
+    except requests.RequestException:
+        # In case of any request exception, return 0
         return 0
-
-
-if __name__ == '__main__':
-    print(number_of_subscribers)('programming')
-    print(number_of_subscribers)('not_a_valid_subreddit')
