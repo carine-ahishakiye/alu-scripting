bcb9cbeb58a6bd5710321f179204c28072453beb
Carine-Ahishakiye <c.yibukabay@alustudent.com>
2024-07-05 10:49:29 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 """
-Contains the top_ten function
+1-main
 """
+import sys
 
-import requests
-
-
-def top_ten(subreddit):
-    """prints the titles of the top ten hot posts for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        print(None)
-    r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
-                     headers={'User-Agent': 'Python/requests:APIproject:\
-                     v1.0.0 (by /u/aaorrico23)'},
-                     params={'limit': 10}).json()
-    posts = r.get('data', {}).get('children', None)
-    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
-        print(None)
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        for post in posts:
-            print(post.get('data', {}).get('title', None))
+        top_ten(sys.argv[1])
